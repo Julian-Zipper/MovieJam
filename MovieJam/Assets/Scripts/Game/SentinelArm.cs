a831130920f2697b1363433927309f7f36e982e7
Julian Kipper <julian.zipper@hva.nl>
2018-02-07 14:10:26 +0100

    Add sentinel prefab

---
+++ MovieJam/Assets/Scripts/Game/SentinelArm.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SentinelArm : MonoBehaviour {
+
+	const string ARM_PART_PREFAB_LOCATION = "Sentinel/arm_part";
+	const int NUMBER_OF_ARM_PARTS = 7;
+	const float ARM_PART_HEIGHT = 0.18f;
+
+	Rigidbody2D hook;
+	GameObject armPart;
+
+	void Start () {
+		armPart = Resources.Load<GameObject> (ARM_PART_PREFAB_LOCATION);
+		hook = transform.GetChild (0).GetComponent<Rigidbody2D>();	// hook is always the first element of an arm object in the hierarchy.
+		GenerateArm ();
+	}
+
+	void GenerateArm()
+	{
+		
+		Rigidbody2D previousRigidBody = hook;	// Default previous part is the first part of the arm, which is always the hook.
+
+		for (int i = 0; i < NUMBER_OF_ARM_PARTS; i++) {
+			GameObject currentPart = GameObject.Instantiate (armPart);
+			currentPart.transform.position = new Vector2 (transform.position.x, transform.position.y - ARM_PART_HEIGHT);
+			HingeJoint2D joint = currentPart.GetComponent<HingeJoint2D> ();
+			joint.connectedBody = previousRigidBody;
+
+			previousRigidBody = currentPart.GetComponent<Rigidbody2D> ();	// set the correct (current) rigidbody for the next arm part to link to.
+		}
+	}
+}
