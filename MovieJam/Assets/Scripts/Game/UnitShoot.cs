a547c6f25cb67864c9eb39fa60542e436859c730
LAPTOP-VA7MNR4P\Quincy <qwijnhard@gmail.com>
2018-02-09 10:51:09 +0100

    Added Unit's targetting + started on shooting

--- MovieJam/Assets/Scripts/Game/UnitShoot.cs
+++ MovieJam/Assets/Scripts/Game/UnitShoot.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitShoot : MonoBehaviour {
+    //find ll sentinels, put them in a list
+    //select a sentinel from the list as target
+    //fire at target
+    //if target stops existing, repeat from step 1
 
-	// Use this for initialization
+    private GameObject[] sentinels;
+    private GameObject target;
+    private float fireDelay;
+    private int random;
+
 	void Start () {
-		
+        FindTarget();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if(target != null && fireDelay <= 0)
+        {
+            Fire();
+        }
+
+        if(target == null)
+        {
+            FindTarget();
+        }
+
+        if(fireDelay > 0)
+        {
+            fireDelay -= Time.deltaTime;
+        }
 	}
+
+    void FindTarget()
+    {
+        sentinels = GameObject.FindGameObjectsWithTag("Sentinel");
+        random = Random.Range(0,sentinels.Length - 1); // Random.Range is inclusive so an index starting with 0 won't like X.length unmodified
+        target = sentinels[random];
+    }
+
+    private void Fire()
+    {
+        Vector3 targetDirection = target.transform.position - transform.position;
+        transform.forward = targetDirection;
+    }
 }
