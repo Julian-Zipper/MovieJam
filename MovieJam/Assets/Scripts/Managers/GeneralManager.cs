93b67841c301bbd5979bee1c3ed77ade4660ec7f
Kevin de Leeuw <lion.drie@gmail.com>
2018-02-10 15:13:37 +0100

    removed unneeded code

--- MovieJam/Assets/Scripts/Managers/GeneralManager.cs
+++ MovieJam/Assets/Scripts/Managers/GeneralManager.cs
@@ -12,6 +12,6 @@ public class GeneralManager : MonoBehaviour
 		ViewManager.Instance.Init ();
 		GameManager.Instance.Init ();
         ShopManager.Instance.Init();
-        MoneyManager.Instance.Init();
+       
 	}
 }
